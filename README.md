a7a950667e306b7d09cffe396514f0c493361fde
Psion <andreas.andra@helsinki.fi>
2018-03-18 18:06:26 +0200

    backend and db config started + eslint added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FullStack-RateMoviesTvShows
+Lyhyt aihekuvaus: Tarkoituksena on toteuttaa jonkun asteinen imdb klooni eli sivusto missä on mahdollisuutta ainakin antaa arvosanoja sekä leffoille että tv sarjoille. Lisää toiminallisuutta tulee jos aika riittää. Frontendin toteutan Reactilla ja backendin Nodella. 
+
+Tuntikirjanpito:
+https://docs.google.com/spreadsheets/d/1kGbM-VnxQZl4DheYler4Sfur5sjv9Cfjcx5K7ru3NQc/edit#gid=0
---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+
+mongoose.connect(config.mongoUrl)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use(middleware.morgan(':method :url :type :status :res[content-length] - :response-time ms'))
+app.use(middleware.tokenExtractor)
+
+app.use(middleware.error)
+
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
+})
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
